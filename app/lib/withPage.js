f844194e2f589182ca26065d5304e526571d578b
Oliver Woodings <oliver.woodings@gmail.com>
2020-01-05 16:29:29 +0000

    Add screenshot-on-error

--- app/lib/withPage.js
+++ app/lib/withPage.js
@@ -1,6 +1,11 @@
 const puppeteer = require('puppeteer')
 const config = require('config')
 const pTimeout = require('p-timeout')
+const path = require('path')
+const fs = require('fs-extra')
+const log = require('./log')('with-page')
+
+const SCREENSHOT_DIR = path.resolve(__dirname, '../../screenshots')
 
 module.exports = function withPage (fn) {
   return async (...args) => {
@@ -11,6 +16,15 @@ module.exports = function withPage (fn) {
       await browser.close()
       return result
     } catch (e) {
+      try {
+        await fs.mkdirp(SCREENSHOT_DIR)
+        await page.screenshot({
+          path: path.resolve(SCREENSHOT_DIR, Date.now() + '.png'),
+          fullPage: true
+        })
+      } catch (e) {
+        log.error('Unable to take screenshot:', e)
+      }
       if (config.puppeteer.closeOnError) {
         await browser.close()
       }
